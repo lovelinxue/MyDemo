7d8048794429c6ce191f0430751662245d0284cf
test <ceshi@songjingnandeMacBook-Air.local>
2015-09-05 09:03:07 +0800

    Initial Commit

---
+++ W_MobileSensors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W_MobileSensors
+//
+//  Created by test on 15/9/5.
+//  Copyright (c) 2015年 WYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W_MobileSensors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W_MobileSensors
+//
+//  Created by test on 15/9/5.
+//  Copyright (c) 2015年 WYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
