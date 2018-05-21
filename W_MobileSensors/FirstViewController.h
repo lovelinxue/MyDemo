96a150274a18ef42a3bbf8fdf848f69434329c8d
854919215@qq.com <854919215@qq.com>
2018-05-21 17:48:47 +0800

    创建控制器

---
+++ W_MobileSensors/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  W_MobileSensors
+//
+//  Created by KweddingForiMac on 2018/5/21.
+//  Copyright © 2018年 WYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ W_MobileSensors/FirstViewController.m
@@ -0,0 +1,37 @@
+//
+//  FirstViewController.m
+//  W_MobileSensors
+//
+//  Created by KweddingForiMac on 2018/5/21.
+//  Copyright © 2018年 WYL. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
