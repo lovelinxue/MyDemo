b394f1df5642585c89daf3f3705d9a904f976daf
854919215@qq.com <854919215@qq.com>
2018-05-21 17:47:16 +0800

    新增文件

---
+++ W_MobileSensors/tool.m
@@ -0,0 +1,21 @@
+//
+//  tool.m
+//  W_MobileSensors
+//
+//  Created by KweddingForiMac on 2018/5/21.
+//  Copyright © 2018年 WYL. All rights reserved.
+//
+
+#import "tool.h"
+
+@implementation tool
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
