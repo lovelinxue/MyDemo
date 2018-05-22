adeb1a17b586ff63f4f5e25f44043a0d4a8255bc
854919215@qq.com <854919215@qq.com>
2018-05-22 10:08:44 +0800

    newF

---
+++ W_MobileSensors/HttpTool.h
@@ -0,0 +1,13 @@
+//
+//  HttpTool.h
+//  W_MobileSensors
+//
+//  Created by KweddingForiMac on 2018/5/22.
+//  Copyright © 2018年 WYL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpTool : NSObject
+
+@end
---
+++ W_MobileSensors/HttpTool.m
@@ -0,0 +1,13 @@
+//
+//  HttpTool.m
+//  W_MobileSensors
+//
+//  Created by KweddingForiMac on 2018/5/22.
+//  Copyright © 2018年 WYL. All rights reserved.
+//
+
+#import "HttpTool.h"
+
+@implementation HttpTool
+
+@end
